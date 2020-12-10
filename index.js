ad0d1fb045235ec9c4815b43f1a571ec0fa86cc2
Logan Smith <logsmith06@gmail.com>
2020-12-10 17:58:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // Code your solutions in this file
 let messages = [];
-function writeCards(names, event)
+function writeCards(names)
 {
   for(let i = 0; i < names.length; i++)
   {
-    messages.push('Thank you '+ ${names[i] 'for the ${event} present!}');
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
   }
 return messages;
+}
+
+function countDown(number)
+{
+  while(number > 0)
+  {
+    console.log(`countdown ${number}`)\
+    number--;
+  }
 }
