c261e1826c4de6defa51f0bc7305411af4790d51
Logan Smith <logsmith06@gmail.com>
2020-12-10 17:58:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+let messages = [];
+function writeCards(names, event)
+{
+  for(let i = 0; i < names.length; i++)
+  {
+    messages.push('Thank you '+ ${names[i] 'for the ${event} present!}');
+  }
+return messages;
+}
